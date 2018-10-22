28b04071c1b2d533bf29d8c35a59fbecf2479a81
chaman bharti <chaman.bharti84@gmail.com>
2018-10-22 15:36:23 +0530

    Added two lines

--- SayHello.java
+++ SayHello.java
@@ -5,8 +5,10 @@ public class SayHello {
     public static void main(String[] args) {
 
         System.out.println("Hello there!");
+	System.out.println("Printint Even Numbers");
        EvenNumbers.printEvenNumber(11);
-       OddNumbers.printOddNumber(11);
+System.out.println("Printint Odd Numbers");
+       OddNumbers.printOddNumber(12);
 
     }
 
