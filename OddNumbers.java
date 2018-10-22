02a91008eccd00a604e3af7771dcdd38be49474b
chaman bharti <chaman.bharti84@gmail.com>
2018-10-22 15:21:01 +0530

    first commit

---
+++ OddNumbers.java
@@ -0,0 +1,13 @@
+
+public class OddNumbers{
+
+    public static void printEvenNumbers(int i) {
+
+        System.out.println("Odd numbers upto "+1);
+	for(int x=1;x<i;x=x+2){
+		System.out.println(x);
+	}
+
+    }
+
+}
